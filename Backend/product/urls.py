91b54a122f8138f7f71b4684e2e12242c50e58a2
mahm0udismail <mahmoudismail9114@gmail.com>
2024-09-19 18:58:57 +0300

    edit and show list prodeuct

--- Backend/product/urls.py
+++ Backend/product/urls.py
@@ -1,7 +1,8 @@
 # product/urls.py
 from django.urls import path
-from .views import ProductDetailAPIView
+from .views import *
 
 urlpatterns = [
+    path('api/products/', ProductListAPIView.as_view(), name='product-list-api'),
     path('api/<slug:category_slug>/<slug:product_slug>/', ProductDetailAPIView.as_view(), name='product-detail-api'),
 ]
--- Backend/product/views.py
+++ Backend/product/views.py
@@ -1,13 +1,31 @@
-# product/views.py
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
 
-class ProductDetailAPIView(generics.RetrieveAPIView):
+class ProductListAPIView(generics.ListAPIView):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    lookup_field = 'slug'  # This should match your model's slug field if needed
+
+class ProductDetailAPIView(generics.GenericAPIView):
+    serializer_class = ProductSerializer
+    lookup_field = 'slug'
 
     def get_object(self):
         category_slug = self.kwargs.get('category_slug')
         product_slug = self.kwargs.get('product_slug')
         return Product.objects.get(slug=product_slug, category__slug=category_slug)
+
+    def get(self, request, *args, **kwargs):
+        product = self.get_object()
+        serializer = self.get_serializer(product)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        product = self.get_object()
+        serializer = self.get_serializer(product, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
