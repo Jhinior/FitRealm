febf1038f8784bbd3ef489cc324a259ab3ddd244
mahm0udismail <mahmoudismail9114@gmail.com>
2024-09-19 19:15:47 +0300

    save mode without slug

--- Backend/product/models.py
+++ Backend/product/models.py
@@ -1,17 +1,25 @@
+from typing import Iterable
 from django.db import models
+from django.utils.text import slugify
+
 
 # Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True, blank=True)
     def __str__(self):
         return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
     
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True, blank=True)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='uploads/', blank=True, null=True)
@@ -21,6 +29,11 @@ class Product(models.Model):
     def __str__(self):
         return self.name
     
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
     def get_absolute_url(self):
         return f'/{self.category.slug}/{self.slug}/'
     
