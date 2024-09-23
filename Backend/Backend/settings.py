406190e2fb7cf78f78913eaa3e45c486b7717037
mahm0udismail <mahmoudismail9114@gmail.com>
2024-09-23 15:35:34 +0300

    creat Blog

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'product',
+    'Blog',
 ]
 
 MIDDLEWARE = [
--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -20,4 +20,5 @@ from django.urls import path , include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('product/', include('product.urls')),
+    path('blog/', include('Blog.urls')),
 ]
