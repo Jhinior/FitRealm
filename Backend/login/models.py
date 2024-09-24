854a673b3c1d7d5d0db6ca3684a215ccbb494886
Mahmoud Sayed <mahmoud.a.elsayed97@outlook.com>
2024-09-24 13:33:48 +0300

    add images field

--- Backend/login/models.py
+++ Backend/login/models.py
@@ -1,46 +1,31 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 from django.contrib.auth.hashers import make_password, check_password
 
-
 # Create your models here.
-
 class Plan(models.Model):
-    
     class PlanNames(models.TextChoices):
         BASIC = "BASIC"
         STANDARD = "STANDARD"
         PREMIUM = "PREMIUM"
         
-    plan_name = models.CharField(max_length=10,choices=PlanNames.choices)    
+    plan_name = models.CharField(max_length=10,choices=PlanNames.choices)
+    description = models.CharField(max_length=250)    
     cost = models.FloatField()
 
 class SuperUser(AbstractBaseUser):
     
     class Gender(models.TextChoices):
-        MALE = "Male" ,  
-        FEMALE = "Female" , 
+        MALE = "Male"   
+        FEMALE = "Female"  
         
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=254, unique=True)
     gender = models.CharField(max_length=6,choices=Gender.choices)
-    password = models.CharField(max_length=128)
-    # image = models.ImageField(upload_to="posts",null=True)
-    payment = models.CharField(max_length=150)
-    
-    def set_password(self, raw_password):
-        self.password = make_password(raw_password)
-        self.save()
-
-    def check_password(self, raw_password):
-        return check_password(raw_password, self.password)
-
-class User(SuperUser):
-    
-    weight = models.FloatField()
-    height = models.FloatField()
-    
+    image = models.ImageField(upload_to="uploads",null=True)
+    phone = ArrayField(models.CharField(max_length=11),size=2)
     
 class Trainer(SuperUser):
     
@@ -50,6 +35,16 @@ class Trainer(SuperUser):
     salary = models.FloatField()
     active_users = models.IntegerField()
     plan = models.ForeignKey(Plan,on_delete=models.SET_NULL, related_name="trainers",null=True)
+
+class User(SuperUser):
+    
+    weight = models.FloatField()
+    height = models.FloatField()
+    plan = models.ForeignKey(Plan,on_delete=models.SET_NULL, related_name="users",null=True)
+    subscribed_date = models.DateField()
+    end_date = models.DateField()
+    trainer = models.ForeignKey(Trainer,on_delete=models.SET_NULL,related_name="users",null=True)
+    
 
 
     
