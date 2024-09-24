db951e9e94f13fa9b75b138bbda8e1fd61e8d084
mahm0udismail <mahmoudismail9114@gmail.com>
2024-09-24 15:15:52 +0300

    add view and serializers and urls

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -20,5 +20,5 @@ from django.urls import path , include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('product/', include('product.urls')),
-    path('blog/', include('Blog.urls')),
+    path('main/', include('login.urls')),
 ]
--- Backend/login/views.py
+++ Backend/login/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
-from .models import Plan
-from .serializers import PlanSerializer
+from .models import Plan, Trainer, User
+from .serializers import PlanSerializer, TrainerSerializer, UserSerializer
 
 # List all plans
 class PlanList(generics.ListCreateAPIView):
@@ -11,3 +11,19 @@ class PlanList(generics.ListCreateAPIView):
 class PlanDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Plan.objects.all()
     serializer_class = PlanSerializer
+
+class TrainerListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Trainer.objects.all()
+    serializer_class = TrainerSerializer
+
+class TrainerDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Trainer.objects.all()
+    serializer_class = TrainerSerializer
+
+class UserListCreateAPIView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
