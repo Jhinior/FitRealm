cb8ea36538fd42c74d72a9e82ecb40546dd77435
mahm0udismail <mahmoudismail9114@gmail.com>
2024-09-24 15:15:52 +0300

    add view and serializers and urls

--- Backend/login/serializers.py
+++ Backend/login/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from .models import Plan
+from .models import Plan, Trainer, User
 
 class PlanSerializer(serializers.ModelSerializer):
     class Meta:
         model = Plan
         fields = ['id', 'plan_name', 'description', 'cost']
+
+class TrainerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Trainer
+        fields = '__all__'  
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__' 
--- Backend/login/urls.py
+++ Backend/login/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
-from .views import PlanList, PlanDetail
+from .views import (PlanList, PlanDetail, TrainerListCreateAPIView,
+    TrainerDetailAPIView,
+    UserListCreateAPIView,
+    UserDetailAPIView,
+    )
 
 urlpatterns = [
     path('plans/', PlanList.as_view(), name='plan-list'),  # For listing and creating plans
     path('plans/<int:pk>/', PlanDetail.as_view(), name='plan-detail'),  # For retrieving, updating, and deleting a specific plan
+    
+    path('trainers/', TrainerListCreateAPIView.as_view(), name='trainer-list-create'),
+    path('trainers/<int:pk>/', TrainerDetailAPIView.as_view(), name='trainer-detail'),
+    
+    path('users/', UserListCreateAPIView.as_view(), name='user-list-create'),
+    path('users/<int:pk>/', UserDetailAPIView.as_view(), name='user-detail'),
 ]
