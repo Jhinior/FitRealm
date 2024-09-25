c12c058b8ab686cf1ba3d4a47c57485d9f0f747e
mahm0udismail <mahmoudismail9114@gmail.com>
2024-09-25 14:11:56 +0300

    add file admin conf

--- Backend/login/models.py
+++ Backend/login/models.py
@@ -1,9 +1,6 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
-from django.contrib.auth.hashers import make_password, check_password
-
-
-# Create your models here.
+from django.contrib.postgres.fields import ArrayField
 
 class Plan(models.Model):
     
@@ -12,7 +9,8 @@ class Plan(models.Model):
         STANDARD = "STANDARD"
         PREMIUM = "PREMIUM"
         
-    plan_name = models.CharField(max_length=10,choices=PlanNames.choices)    
+    plan_name = models.CharField(max_length=10, choices=PlanNames.choices)
+    description = models.CharField(max_length=250)    
     cost = models.FloatField()
 
 class SuperUser(AbstractBaseUser):
@@ -24,40 +22,28 @@ class SuperUser(AbstractBaseUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=254, unique=True)
-    gender = models.CharField(max_length=6,choices=Gender.choices)
-    password = models.CharField(max_length=128)
-    # image = models.ImageField(upload_to="posts",null=True)
-    payment = models.CharField(max_length=150)
-    
-    def set_password(self, raw_password):
-        self.password = make_password(raw_password)
-        self.save()
+    gender = models.CharField(max_length=6, choices=Gender.choices)
+    image = models.ImageField(upload_to="uploads", null=True)
+    phone = ArrayField(models.CharField(max_length=11), size=2)
 
-    def check_password(self, raw_password):
-        return check_password(raw_password, self.password)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
-class User(SuperUser):
-    
-    weight = models.FloatField()
-    height = models.FloatField()
-    
-    
+    class Meta:
+        abstract = False
+
 class Trainer(SuperUser):
-    
     reviews = models.CharField(max_length=250)
     years_of_experience = models.IntegerField()
     avg_rating = models.FloatField()
     salary = models.FloatField()
     active_users = models.IntegerField()
-    plan = models.ForeignKey(Plan,on_delete=models.SET_NULL, related_name="trainers",null=True)
+    plan = models.ForeignKey(Plan, on_delete=models.SET_NULL, related_name="trainers", null=True)
 
-
-    
-    
-    
-    
-    
-    
-   
-    
-    
+class User(SuperUser):
+    weight = models.FloatField()
+    height = models.FloatField()
+    plan = models.ForeignKey(Plan, on_delete=models.SET_NULL, related_name="users", null=True)
+    subscribed_date = models.DateField()
+    end_date = models.DateField()
+    assigned_trainer = models.ForeignKey(Trainer, on_delete=models.SET_NULL, related_name="users", null=True)
