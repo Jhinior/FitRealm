6f18303b30d2b944ad8a167ba2351cfcdec582bd
mahm0udismail <mahmoudismail9114@gmail.com>
2024-09-25 15:14:16 +0300

    Resolved merge conflicts in models.py

--- Backend/login/models.py
+++ Backend/login/models.py
@@ -1,24 +1,15 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
-<<<<<<< HEAD
-
-=======
-from django.contrib.auth.hashers import make_password, check_password
 
 # Create your models here.
->>>>>>> 854a673 (add images field)
 class Plan(models.Model):
     class PlanNames(models.TextChoices):
         BASIC = "BASIC"
         STANDARD = "STANDARD"
         PREMIUM = "PREMIUM"
         
-<<<<<<< HEAD
     plan_name = models.CharField(max_length=10, choices=PlanNames.choices)
-=======
-    plan_name = models.CharField(max_length=10,choices=PlanNames.choices)
->>>>>>> 854a673 (add images field)
     description = models.CharField(max_length=250)    
     cost = models.FloatField()
     image = models.ImageField(upload_to="plans", null=True)
@@ -33,9 +24,8 @@ class SuperUser(AbstractBaseUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=254, unique=True)
-<<<<<<< HEAD
     gender = models.CharField(max_length=6, choices=Gender.choices)
-    image = models.ImageField(upload_to="SuperUser", null=True)
+    image = models.ImageField(upload_to="uploads", null=True)  # Changed upload path to 'uploads'
     phone = ArrayField(models.CharField(max_length=11), size=2)
 
     USERNAME_FIELD = 'email'
@@ -44,12 +34,7 @@ class SuperUser(AbstractBaseUser):
     class Meta:
         abstract = False
 
-=======
-    gender = models.CharField(max_length=6,choices=Gender.choices)
-    image = models.ImageField(upload_to="uploads",null=True)
-    phone = ArrayField(models.CharField(max_length=11),size=2)
-    
->>>>>>> 854a673 (add images field)
+
 class Trainer(SuperUser):
     reviews = models.CharField(max_length=250)
     years_of_experience = models.IntegerField()
@@ -59,31 +44,9 @@ class Trainer(SuperUser):
     plan = models.ForeignKey(Plan, on_delete=models.SET_NULL, related_name="trainers", null=True)
 
 class User(SuperUser):
-<<<<<<< HEAD
     weight = models.FloatField()
     height = models.FloatField()
     plan = models.ForeignKey(Plan, on_delete=models.SET_NULL, related_name="users", null=True)
     subscribed_date = models.DateField()
     end_date = models.DateField()
-    assigned_trainer = models.ForeignKey(Trainer, on_delete=models.SET_NULL, related_name="users", null=True)
-=======
-    
-    weight = models.FloatField()
-    height = models.FloatField()
-    plan = models.ForeignKey(Plan,on_delete=models.SET_NULL, related_name="users",null=True)
-    subscribed_date = models.DateField()
-    end_date = models.DateField()
-    trainer = models.ForeignKey(Trainer,on_delete=models.SET_NULL,related_name="users",null=True)
-    
-
-
-    
-    
-    
-    
-    
-    
-   
-    
-    
->>>>>>> 854a673 (add images field)
+    trainer = models.ForeignKey(Trainer, on_delete=models.SET_NULL, related_name="users", null=True)
