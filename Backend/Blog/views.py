e09757754bc85598b875ea863aa8a088373fbab1
mahm0udismail <mahmoudismail9114@gmail.com>
2024-09-26 15:44:40 +0300

    add profile

--- Backend/Blog/views.py
+++ Backend/Blog/views.py
@@ -1,6 +1,6 @@
-from rest_framework import viewsets
-from .models import User, Post, Category, Comment
-from .serializers import UserSerializer, PostSerializer, CategorySerializer, CommentSerializer
+from rest_framework import viewsets, permissions
+from .models import User, Post, Category, Comment, Profile
+from .serializers import UserSerializer, PostSerializer, CategorySerializer, CommentSerializer, ProfileSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -17,3 +17,13 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    serializer_class = ProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]  # Optional: require authentication
+
+    def get_queryset(self):
+        user_id = self.kwargs.get('user_id')
+        if user_id is not None:
+            return Profile.objects.filter(user__id=user_id)
+        return Profile.objects.all()  # Return all profiles if no user_id is provided
