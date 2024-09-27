20632ca6021f6f6dea0d91233520e7ee6893414b
mahm0udismail <mahmoudismail9114@gmail.com>
2024-09-27 20:37:33 +0300

    make signup and login in backend

--- Backend/login/models.py
+++ Backend/login/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
 
 class Plan(models.Model):
     class PlanNames(models.TextChoices):
@@ -13,25 +12,35 @@ class Plan(models.Model):
     cost = models.FloatField()
     image = models.ImageField(upload_to="plans", null=True)
 
-
 class SuperUser(AbstractBaseUser):
-    
     class Gender(models.TextChoices):
-        MALE = "Male"   
-        FEMALE = "Female"  
-        
+        MALE = "Male"
+        FEMALE = "Female"
+
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=254, unique=True)
     gender = models.CharField(max_length=6, choices=Gender.choices)
     image = models.ImageField(upload_to="SuperUser", null=True)
-    phone = ArrayField(models.CharField(max_length=11), size=2)
-
+    phone = models.CharField(max_length=15)  # Adjusted to CharField for a single phone number
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
 
     class Meta:
         abstract = False
+
+
+class User(SuperUser):
+    weight = models.FloatField(null=True, blank=True)
+    height = models.FloatField(null=True, blank=True)
+    plan = models.ForeignKey(Plan, on_delete=models.SET_NULL, related_name="users", null=True)
+    subscribed_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    assigned_trainer = models.ForeignKey('Trainer', on_delete=models.SET_NULL, related_name="users", null=True)
+
+    def save(self, *args, **kwargs):
+        # Additional logic for saving user if needed
+        super(User, self).save(*args, **kwargs)
 
 class Trainer(SuperUser):
     reviews = models.CharField(max_length=250)
@@ -40,11 +49,3 @@ class Trainer(SuperUser):
     salary = models.FloatField()
     active_users = models.IntegerField()
     plan = models.ForeignKey(Plan, on_delete=models.SET_NULL, related_name="trainers", null=True)
-
-class User(SuperUser):
-    weight = models.FloatField()
-    height = models.FloatField()
-    plan = models.ForeignKey(Plan, on_delete=models.SET_NULL, related_name="users", null=True)
-    subscribed_date = models.DateField()
-    end_date = models.DateField()
-    assigned_trainer = models.ForeignKey(Trainer, on_delete=models.SET_NULL, related_name="users", null=True)
