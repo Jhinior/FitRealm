628498a1fb038cea76215cb49a4f8ae3b61c61e4
mahm0udismail <mahmoudismail9114@gmail.com>
2024-09-28 22:04:42 +0300

    edit user in order

--- Backend/order/models.py
+++ Backend/order/models.py
@@ -1,10 +1,11 @@
 # models.py
 from django.db import models
 from django.contrib.auth import get_user_model
+from login.models import User
 from product.models import Product
 
 class Order(models.Model):
-    user = models.ForeignKey(get_user_model(), related_name='orders', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='orders', on_delete=models.CASCADE) 
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
