a7931ff9991f3e832d205156bf09421461474fd9
Omar Yehia <omaryehia755@gmail.com>
2024-09-28 03:53:30 +0300

    add home page in the Frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
-
+// import ProgramsList from "./components/ProgramsList";
+// import OurPrograms from "./components/OurPrograms";
+// import Authintication from './components/Authintication';
+// import ProgramOne from "./components/ProgramOne";
+// import Blogs from "./components/Blogs";
 import Navbar from './components/Navbar'
 import ProductList from './components/Products/ProductList'
 import ProductDetail from './components/Products/ProductDetail'
 import Cart from './components/Products/Cart'
 import Checkout from './components/Products/Checkout'
+import HomePage from './components/HomePage'
 
 
 
@@ -27,6 +32,7 @@ function App() {
             <Route path="/product/:name" element={<ProductDetail />} />
             <Route path="/cart" element={<Cart/>} />
             <Route path="/Checkout" element={<Checkout/>} />
+            <Route path="/Home" element={<HomePage/>} />
 
 
             {/* Add more routes as needed */}
