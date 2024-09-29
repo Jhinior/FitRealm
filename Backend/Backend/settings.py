f18e640cdc82c96699de4a623d45862685e87bdd
ahmad <ahmadabdulmuniem8@gmail.com>
2024-09-29 07:05:23 +0300

    changing the email and password

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -151,5 +151,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.example.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'muharramahmad770@gmail.com'
-EMAIL_HOST_PASSWORD = 'Ahmadabdulmuniem-1999'
+EMAIL_HOST_USER = 'example@gmail.com'
+EMAIL_HOST_PASSWORD = 'any'
