f4113af2ad8068a1a4ae93ff076c26ed8c0baab8
ahmad <ahmadabdulmuniem8@gmail.com>
2024-10-01 00:12:21 +0300

    solving the conflicts and adjusting the css styles, also working on the contact form.

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -156,6 +156,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'muharramahmad770@gmail.com'  
