cf7caa926072f29f6246bb3a7d4783a4fd558b44
mahm0udismail <mahmoudismail9114@gmail.com>
2024-10-07 16:44:20 +0300

    add filter in subscription

--- Backend/plans/api.py
+++ Backend/plans/api.py
@@ -1,6 +1,6 @@
 from rest_framework import generics, viewsets
 from .models import Plan, Subscription
-from .serializers import PlanSerializer, SubscriptionSerializer    
+from .serializers import PlanSerializer, SubscriptionSerializer, TrainerSerializer
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from login.models import Trainer
@@ -19,12 +19,4 @@ class PlanDetail(generics.RetrieveUpdateDestroyAPIView):
 class SubscriptionViewSet(viewsets.ModelViewSet):
     queryset = Subscription.objects.all()
     serializer_class = SubscriptionSerializer
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
 
-    @action(detail=False, methods=['get'])
-    def active_users_min_10(self, request):
-        # Filter trainers with at least 10 active users
-        trainers = Trainer.objects.filter(active_users__gte=1)
-        serializer = self.get_serializer(trainers, many=True)
-        return Response(serializer.data)
--- Backend/plans/serializers.py
+++ Backend/plans/serializers.py
@@ -8,20 +8,13 @@ class PlanSerializer(serializers.ModelSerializer):
     class Meta:
         model = Plan
         fields = '__all__'
-class SubscriptionSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField()  # Showing related user as a string
-    trainerswithmin_active_users = serializers.SerializerMethodField()  # Custom field
 
+class SubscriptionSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()  # Display user as a string representation
+    trainer = serializers.PrimaryKeyRelatedField(queryset=Trainer.objects.filter(active_users__lt=10))
     class Meta:
         model = Subscription
         fields = '__all__'
-
-    # Correct method name
-    def get_trainers_with_min_active_users(self, obj):
-        # Filter trainers with at least 10 active users
-        trainers = Trainer.objects.filter(active_users__gte=10)
-        # You can serialize the trainer data or just return their names
-        return [f"{trainer.first_name} {trainer.last_name}" for trainer in trainers]
         
 class TrainerSerializer(serializers.ModelSerializer):
     class Meta:
