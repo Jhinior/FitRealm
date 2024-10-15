917f9b47415a04b645778bc064d910464e540c66
Omar Yehia <omaryehia755@gmail.com>
2024-10-15 23:15:36 +0300

    editing program details page and all programs page

--- Frontend/src/components/ProgramOne.jsx
+++ Frontend/src/components/ProgramOne.jsx
@@ -1,93 +1,3 @@
-// import { useParams, Link, useNavigate } from 'react-router-dom';
-// import { useEffect, useState } from 'react';
-// import { FaDollarSign } from 'react-icons/fa';
-
-// import ropeJumping from "../assets/images/ropeJumbing.mp4";
-// import axiosInstance from '../axios';
-// import '../assets/styles/ProgramOne.css';
-
-// function ProgramOne() {
-//     const { id } = useParams(); 
-//     const [program, setProgram] = useState(null); 
-//     const [error, setError] = useState(null);
-//     const navigate = useNavigate();
-
-//     const handleMakePayment = () => {
-//       navigate(`/subscribe/${program.id}`, { state: { planName: program.plan_name, price: program.cost } });
-//     };
-
-//     useEffect(() => {
-//         const fetchPlanDetail = async () => {
-//           try {
-//             const response = await axiosInstance.get(`/api/plans/${id}/`);
-//             setProgram(response.data); 
-//           } catch (error) {
-//             console.error('Error fetching plan detail:', error);
-//             setError('Failed to load program details.');
-//           }
-//         };
-      
-//         fetchPlanDetail();
-//     }, [id]);
-
-//     if (error) {
-//         return <p>{error}</p>;
-//     }
-
-//     if (!program) {
-//         return <p>Loading...</p>;
-//     }
-
-//     return (
-//       <>
-//         <video
-//           autoPlay
-//           loop
-//           muted
-//           id="video"
-//           className="rope-program-one"
-//           src={ropeJumping}
-//           alt="Rope-Jumping-video"
-//         />
-//         <div className="container-program-one">
-//           <div className="head-program-one">
-//             <h1 className="header">PROGRAM DETAILS</h1>
-//           </div>
-//           <div className="p-program-one">
-//             <p className="paragraph">
-//               Here are the details for the {program.plan_name} program.
-//             </p>
-//           </div>
-//           <div className="program-card-one">
-//             <div className="program-detail">
-//               <img
-//                 className="people"
-//                 src={program.image}
-//                 alt={program.plan_name}
-//               />
-//               <h3>{program.name}</h3>
-//               <div className='program-one-cont'>
-//                <p>Price: {program.cost} <FaDollarSign/> </p>
-//               <p className="desc-program-one">{program.description}</p>
-
-//               <div className="program-detail-buttons">
-//                 <Link to="/programslist">
-//                   <button className="v-l-details">Back to Programs</button>
-//                 </Link>
-//                 <button className="v-r-details" onClick={handleMakePayment}>Want to Make a Payment</button>
-//               </div>
-//               </div>
-              
-//             </div>
-//           </div>
-//         </div>
-//       </>
-//     );
-// }
-
-// export default ProgramOne;
-
-
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { FaDollarSign } from 'react-icons/fa';
@@ -151,13 +61,13 @@ function ProgramOne() {
         />
         <div className="container-program-one">
           <div className="head-program-one">
-            <h1 className="header">PROGRAM DETAILS</h1>
+            <h1 className="header">{program.plan_name} PROGRAM DETAILS</h1>
           </div>
-          <div className="p-program-one">
+          {/* <div className="p-program-one">
             <p className="paragraph">
               Here are the details for the {program.plan_name} program.
             </p>
-          </div>
+          </div> */}
           <div className="program-card-one">
             <div className="program-detail">
               <img
@@ -165,16 +75,16 @@ function ProgramOne() {
                 src={program.image}
                 alt={program.plan_name}
               />
-              <h3>{program.name}</h3>
               <div className='program-one-cont'>
-               <p>Price: {program.cost} <FaDollarSign/> </p>
-              <p className="desc-program-one">{program.description}</p>
+                <p>Price: {program.cost} <FaDollarSign/> </p>
+                <p className="desc-program-one">{program.description}</p>
+              
 
               <div className="program-detail-buttons">
                 <Link to="/programslist">
-                  <button className="v-l-details">Back to Programs</button>
+                  <button className="v-l-details">All Programs</button>
                 </Link>
-                <button className="v-r-details" onClick={handleMakePayment}>Want to Make a Payment</button>
+                <button className="v-r-details" onClick={handleMakePayment}>Claim Your Spot</button>
               </div>
               </div>
               
