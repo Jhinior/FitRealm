5546a8657e165aebd53468a024c9931160198228
mahm0udismail <mahmoudismail9114@gmail.com>
2024-10-25 12:13:59 +0300

    add Bookmark in backend

--- Backend/Blog/models.py
+++ Backend/Blog/models.py
@@ -1,11 +1,134 @@
+# from django.db import models
+# from django.utils.text import slugify
+# from django.utils.html import mark_safe
+# from shortuuid.django_fields import ShortUUIDField
+# from django.db.models.signals import post_save  # Import post_save
+# import shortuuid
+# from login.models import Trainer, User,SuperUser
+
+# class Profile(models.Model):
+#     user = models.OneToOneField(SuperUser, on_delete=models.CASCADE)
+#     image = models.FileField(upload_to="image", default="default/default-user.jpg", null=True, blank=True)
+#     full_name = models.CharField(max_length=100, null=True, blank=True)
+#     bio = models.TextField(null=True, blank=True)
+#     about = models.TextField(null=True, blank=True)
+#     author = models.BooleanField(default=False)
+#     country = models.CharField(max_length=100, null=True, blank=True)
+#     facebook = models.CharField(max_length=100, null=True, blank=True)
+#     twitter = models.CharField(max_length=100, null=True, blank=True)
+#     date = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return str(self.full_name) if self.full_name else str(self.user.email)
+    
+#     def save(self, *args, **kwargs):
+#         if not self.full_name:
+#             self.full_name = self.user.first_name + " " + self.user.last_name
+#         super(Profile, self).save(*args, **kwargs)
+
+#     def thumbnail(self):
+#         return mark_safe('<img src="/media/%s" width="50" height="50" style="border-radius: 30px; object-fit: cover;" />' % self.image)
+
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
+
+# def save_user_profile(sender, instance, **kwargs):
+#     instance.profile.save()
+
+# # Connect the signals with User model
+# post_save.connect(create_user_profile, sender=User)
+# post_save.connect(save_user_profile, sender=User)
+
+# class Category(models.Model):
+#     title = models.CharField(max_length=100)
+#     image = models.FileField(upload_to="image", null=True, blank=True)
+#     slug = models.SlugField(unique=True, null=True, blank=True)
+
+#     def __str__(self):
+#         return self.title
+
+#     def save(self, *args, **kwargs):
+#         if not self.slug:
+#             self.slug = slugify(self.title)
+#         super(Category, self).save(*args, **kwargs)
+    
+#     def post_count(self):
+#         return Post.objects.filter(category=self).count()
+
+# class Post(models.Model):
+#     STATUS = ( 
+#         ("Active", "Active"), 
+#         ("Draft", "Draft"),
+#         ("Disabled", "Disabled"),
+#     )
+
+#     user = models.ForeignKey(Trainer, on_delete=models.CASCADE)
+#     title = models.CharField(max_length=100)
+#     image = models.FileField(upload_to="image", null=True, blank=True)
+#     description = models.TextField(null=True, blank=True)
+#     tags = models.CharField(max_length=100)
+#     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='posts')
+#     status = models.CharField(max_length=100, choices=STATUS, default="Active")
+#     view = models.IntegerField(default=0)
+#     likes = models.ManyToManyField(User, blank=True, related_name="likes_user")
+#     slug = models.SlugField(unique=True, null=True, blank=True)
+#     date = models.DateTimeField(auto_now_add=True)
+    
+#     def __str__(self):
+#         return self.title
+
+#     def save(self, *args, **kwargs):
+#         if not self.slug:
+#             self.slug = slugify(self.title) + "-" + shortuuid.uuid()[:2]
+#         super(Post, self).save(*args, **kwargs)
+    
+#     def comments(self):
+#         return Comment.objects.filter(post=self).order_by("-id")
+
+# class Comment(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+#     # name = models.CharField(max_length=100)
+#     email = models.CharField(max_length=100)
+#     comment = models.TextField()
+#     reply = models.TextField(null=True, blank=True)
+#     date = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return f"{self.post.title} - {self.name}"
+
+# class Bookmark(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+#     date = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return f"{self.post.title} - {self.user.email}"
+
+# class Notification(models.Model):
+#     NOTI_TYPE = ( ("Like", "Like"), ("Comment", "Comment"), ("Bookmark", "Bookmark"))
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+#     type = models.CharField(max_length=100, choices=NOTI_TYPE)
+#     seen = models.BooleanField(default=False)
+#     date = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return f"{self.type} - {self.post.title if self.post else 'Notification'}"
+
+
+
 from django.db import models
 from django.utils.text import slugify
 from django.utils.html import mark_safe
 from shortuuid.django_fields import ShortUUIDField
-from django.db.models.signals import post_save  # Import post_save
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 import shortuuid
-from login.models import Trainer, User,SuperUser
+from login.models import Trainer, User, SuperUser
 
+# Profile Model
 class Profile(models.Model):
     user = models.OneToOneField(SuperUser, on_delete=models.CASCADE)
     image = models.FileField(upload_to="image", default="default/default-user.jpg", null=True, blank=True)
@@ -23,23 +146,23 @@ class Profile(models.Model):
     
     def save(self, *args, **kwargs):
         if not self.full_name:
-            self.full_name = self.user.first_name + " " + self.user.last_name
+            self.full_name = f"{self.user.first_name} {self.user.last_name}"
         super(Profile, self).save(*args, **kwargs)
 
     def thumbnail(self):
         return mark_safe('<img src="/media/%s" width="50" height="50" style="border-radius: 30px; object-fit: cover;" />' % self.image)
 
+# Signal to create or save Profile upon User creation or save
+@receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
+@receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
 
-# Connect the signals with User model
-post_save.connect(create_user_profile, sender=User)
-post_save.connect(save_user_profile, sender=User)
-
+# Category Model
 class Category(models.Model):
     title = models.CharField(max_length=100)
     image = models.FileField(upload_to="image", null=True, blank=True)
@@ -54,11 +177,12 @@ class Category(models.Model):
         super(Category, self).save(*args, **kwargs)
     
     def post_count(self):
-        return Post.objects.filter(category=self).count()
+        return self.posts.count()
 
+# Post Model
 class Post(models.Model):
-    STATUS = ( 
-        ("Active", "Active"), 
+    STATUS_CHOICES = (
+        ("Active", "Active"),
         ("Draft", "Draft"),
         ("Disabled", "Disabled"),
     )
@@ -69,12 +193,12 @@ class Post(models.Model):
     description = models.TextField(null=True, blank=True)
     tags = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='posts')
-    status = models.CharField(max_length=100, choices=STATUS, default="Active")
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default="Active")
     view = models.IntegerField(default=0)
-    likes = models.ManyToManyField(User, blank=True, related_name="likes_user")
+    likes = models.ManyToManyField(User, blank=True, related_name="liked_posts")
     slug = models.SlugField(unique=True, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return self.title
 
@@ -84,20 +208,21 @@ class Post(models.Model):
         super(Post, self).save(*args, **kwargs)
     
     def comments(self):
-        return Comment.objects.filter(post=self).order_by("-id")
+        return self.comment_set.order_by("-id")
 
+# Comment Model
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    # name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     comment = models.TextField()
     reply = models.TextField(null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.post.title} - {self.name}"
+        return f"{self.post.title} - {self.user.first_name}"
 
+# Bookmark Model
 class Bookmark(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
@@ -106,11 +231,17 @@ class Bookmark(models.Model):
     def __str__(self):
         return f"{self.post.title} - {self.user.email}"
 
+# Notification Model
 class Notification(models.Model):
-    NOTI_TYPE = ( ("Like", "Like"), ("Comment", "Comment"), ("Bookmark", "Bookmark"))
+    NOTIFICATION_TYPES = (
+        ("Like", "Like"),
+        ("Comment", "Comment"),
+        ("Bookmark", "Bookmark"),
+    )
+    
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    type = models.CharField(max_length=100, choices=NOTI_TYPE)
+    type = models.CharField(max_length=100, choices=NOTIFICATION_TYPES)
     seen = models.BooleanField(default=False)
     date = models.DateTimeField(auto_now_add=True)
 
