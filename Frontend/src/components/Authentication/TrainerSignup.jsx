2d4bbeb4d3494ff4b74a75f23dbebdf50c6284a9
ahmad <ahmadabdulmuniem8@gmail.com>
2024-10-30 22:26:23 +0300

    adjusting the notification.

--- Frontend/src/components/Authentication/TrainerSignup.jsx
+++ Frontend/src/components/Authentication/TrainerSignup.jsx
@@ -606,6 +606,289 @@
 // export default TrainerLogin;
 
 
+// import React, { useContext, useState } from "react";
+// import { AuthContext } from "./TrainerAuthContext";
+// import { useFormik } from "formik";
+// import "../../assets/styles/Authentication/register.css";
+// import { TrainerSignupSchema } from "./Schema";
+// import Logo from "./Logo";
+// import Trainer_Signup from "./utils/TrainerSignup";
+// import "@fortawesome/fontawesome-free/css/all.min.css"; // Import Font Awesome
+
+// const onSubmit = async (values, actions) => {
+//   try {
+//     const url = "http://127.0.0.1:8000/main/signup/trainer/";
+//     console.log(values);
+//     const response = await Trainer_Signup(values, url);
+
+//     if (response.success) {
+//       console.log("Sign-up successful:", response.message);
+//       actions.resetForm();
+//       window.location.href = "/home";
+//     } else {
+//       if (response.email === "super user with this email already exists.") {
+//         const P = document.querySelector(".invalid-email");
+//         P.setAttribute("id", "invalid-email");
+//       }
+//     }
+//   } catch (error) {
+//     console.error("Error during sign-up:", error);
+//   } finally {
+//     actions.setSubmitting(false);
+//   }
+// };
+
+// const TrainerSignup = () => {
+//   const [showPassword, setShowPassword] = useState(false);
+//   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+//   const {
+//     values,
+//     errors,
+//     touched,
+//     isSubmitting,
+//     handleChange,
+//     handleBlur,
+//     handleSubmit,
+//     setFieldValue,
+//   } = useFormik({
+//     initialValues: {
+//       firstname: "",
+//       lastname: "",
+//       email: "",
+//       phone: "",
+//       years_of_experience: "",
+//       passwd: "",
+//       confirmpasswd: "",
+//       sign_gender: "",
+//       photo: null,
+//     },
+//     validationSchema: TrainerSignupSchema,
+//     onSubmit,
+//   });
+
+//   const { setWhoLogin } = useContext(AuthContext);
+
+//   const handleFileChange = (event) => {
+//     setFieldValue("photo", event.target.files[0]);
+//   };
+
+//   const x = () => {
+//     const P = document.querySelector("#invalid-email");
+//     if (P) {
+//       P.removeAttribute("id");
+//     }
+//   };
+
+//   return (
+//     <>
+//       <Logo />
+//       <div className="trainer-container">
+//         <div className="row"></div>
+//         <div className="col-md-12 cardd signup-form">
+//           <form onSubmit={handleSubmit} className="box">
+//             <h1>Trainer Registration</h1>
+//             <p className="text-muted">
+//               {" "}
+//               Please enter your data for registration!
+//             </p>
+//             <label htmlFor="firstname">First Name:</label>
+
+
+//             <input
+//               type="text"
+//               id="firstname"
+//               placeholder="First Name"
+//               value={values.firstname}
+//               onChange={handleChange}
+//               onBlur={handleBlur}
+//               className={
+//                 errors.firstname && touched.firstname ? "input-error" : ""
+//               }
+//             />
+//             {errors.firstname && touched.firstname && (
+//               <p className="error">{errors.firstname}</p>
+//             )}
+//                                     <label htmlFor="lastname">Last Name:</label>
+
+//             <input
+//               type="text"
+//               id="lastname"
+//               placeholder="Last Name"
+//               value={values.lastname}
+//               onChange={handleChange}
+//               onBlur={handleBlur}
+//               className={
+//                 errors.lastname && touched.lastname ? "input-error" : ""
+//               }
+//             />
+//             {errors.lastname && touched.lastname && (
+//               <p className="error">{errors.lastname}</p>
+//             )}
+//                         <label htmlFor="email">Email:</label>
+
+//             <input
+//               type="email"
+//               id="email"
+//               placeholder="Email"
+//               value={values.email}
+//               onChange={handleChange}
+//               onBlur={handleBlur}
+//               className={errors.email && touched.email ? "input-error" : ""}
+//             />
+//             <p className="invalid-email" style={{ display: "none" }}>
+//               This email is already used
+//             </p>
+//             {errors.email && touched.email && (
+//               <p className="error">{errors.email}</p>
+//             )}
+//                         <label htmlFor="phone">Phone:</label>
+
+//             <input
+//               type="text"
+//               id="phone"
+//               placeholder="Phone"
+//               value={values.phone}
+//               onChange={handleChange}
+//               onBlur={handleBlur}
+//               className={errors.phone && touched.phone ? "input-error" : ""}
+//             />
+//             {errors.phone && touched.phone && (
+//               <p className="error">{errors.phone}</p>
+//             )}
+//                         <label htmlFor="years_of_experience">Experience:</label>
+
+//             <input
+//               type="number" 
+//               id="years_of_experience"
+//               placeholder="Years of Experience"
+//               value={values.years_of_experience}
+//               onChange={handleChange}
+//               onBlur={handleBlur}
+//               className=""
+//             />
+//             {errors.years_of_experience && touched.years_of_experience && (
+//               <p className="error">{errors.years_of_experience}</p>
+//             )}
+//             <div className="password-container">
+//               <div
+//                 className="password-container"
+//                 style={{ position: "relative" }}
+//               >
+//                             <label htmlFor="passwd">Password:</label>
+
+//                 <input
+//                   type={showPassword ? "text" : "password"} 
+//                   id="passwd"
+//                   placeholder="Password"
+//                   value={values.passwd}
+//                   onChange={handleChange}
+//                   onBlur={handleBlur}
+//                   className={
+//                     errors.passwd && touched.passwd ? "input-error" : ""
+//                   }
+//                 />
+//                 <button
+//                   type="button"
+//                   onClick={() => setShowPassword(!showPassword)}
+//                   className="btn btn-link toggle-password" 
+//                   style={{
+//                     position: "absolute",
+//                     right: "85px",
+//                     top: "72%",
+//                     transform: "translateY(-50%)",
+//                   }} 
+//                 >
+//                   {showPassword ? (
+//                     <i className="fa-solid fa-eye-slash"></i> 
+//                   ) : (
+//                     <i className="fa-solid fa-eye"></i> 
+//                   )}
+//                 </button>
+//               </div>
+//             </div>
+//             {errors.passwd && touched.passwd && (
+//               <p className="error">{errors.passwd}</p>
+//             )}
+//             <div className="password-container">
+//               <div
+//                 className="password-container"
+//                 style={{ position: "relative" }}
+//               >
+//                                             <label htmlFor="confirmpasswd">Confirm Password:</label>
+
+//                 <input
+//                   type={showConfirmPassword ? "text" : "password"} 
+//                   id="confirmpasswd"
+//                   placeholder="Confirm Password"
+//                   value={values.confirmpasswd}
+//                   onChange={handleChange}
+//                   onBlur={handleBlur}
+//                   className={
+//                     errors.confirmpasswd && touched.confirmpasswd
+//                       ? "input-error"
+//                       : ""
+//                   }
+//                 />
+//                 <button
+//                   type="button"
+//                   onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+//                   className="btn btn-link toggle-password" 
+//                   style={{
+//                     position: "absolute",
+//                     right: "85px",
+//                     top: "72%",
+//                     transform: "translateY(-50%)",
+//                   }} 
+//                 >
+//                   {showConfirmPassword ? (
+//                     <i className="fa-solid fa-eye-slash"></i> 
+//                   ) : (
+//                     <i className="fa-solid fa-eye"></i> 
+//                   )}
+//                 </button>
+//               </div>
+//             </div>
+//             {errors.confirmpasswd && touched.confirmpasswd && (
+//               <p className="error">{errors.confirmpasswd}</p>
+//             )}
+                        
+//             <label className="custom-file-upload">
+//               <input type="file" id="photo" onChange={handleFileChange} />
+//               Your Image
+//             </label>
+//             <label className="custom-file-upload">
+//               <input type="file" id="photo" onChange={handleFileChange} />
+//               Your Certificate
+//             </label>
+
+//             <input
+//               type="submit"
+//               disabled={isSubmitting}
+//               value="Sign up"
+//               href="#"
+//             />
+//             <div id="login-google">
+//               <i className="fa-brands fa-google"></i>
+//             </div>
+//             <span className="spn">
+//               Already a member?
+//               <a
+//                 className="login text-muted"
+//                 onClick={() => setWhoLogin("trainer-login")}
+//               >
+//                 &nbsp;Login
+//               </a>
+//             </span>
+//           </form>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default TrainerSignup;
+
 import React, { useContext, useState } from "react";
 import { AuthContext } from "./TrainerAuthContext";
 import { useFormik } from "formik";
@@ -614,6 +897,8 @@ import { TrainerSignupSchema } from "./Schema";
 import Logo from "./Logo";
 import Trainer_Signup from "./utils/TrainerSignup";
 import "@fortawesome/fontawesome-free/css/all.min.css"; // Import Font Awesome
+import { ToastContainer, toast } from 'react-toastify'; // Import Toast
+import 'react-toastify/dist/ReactToastify.css'; // Import Toast CSS
 
 const onSubmit = async (values, actions) => {
   try {
@@ -624,7 +909,11 @@ const onSubmit = async (values, actions) => {
     if (response.success) {
       console.log("Sign-up successful:", response.message);
       actions.resetForm();
-      window.location.href = "/home";
+      toast.success("Your information is under review!"); // Show success toast
+      setTimeout(() => {
+        window.location.href = "/home"; // Redirect to home page
+      }, 3000);
+      //window.location.href = "/home";
     } else {
       if (response.email === "super user with this email already exists.") {
         const P = document.querySelector(".invalid-email");
@@ -693,8 +982,6 @@ const TrainerSignup = () => {
               Please enter your data for registration!
             </p>
             <label htmlFor="firstname">First Name:</label>
-
-
             <input
               type="text"
               id="firstname"
@@ -709,8 +996,7 @@ const TrainerSignup = () => {
             {errors.firstname && touched.firstname && (
               <p className="error">{errors.firstname}</p>
             )}
-                                    <label htmlFor="lastname">Last Name:</label>
-
+            <label htmlFor="lastname">Last Name:</label>
             <input
               type="text"
               id="lastname"
@@ -725,8 +1011,7 @@ const TrainerSignup = () => {
             {errors.lastname && touched.lastname && (
               <p className="error">{errors.lastname}</p>
             )}
-                        <label htmlFor="email">Email:</label>
-
+            <label htmlFor="email">Email:</label>
             <input
               type="email"
               id="email"
@@ -742,8 +1027,7 @@ const TrainerSignup = () => {
             {errors.email && touched.email && (
               <p className="error">{errors.email}</p>
             )}
-                        <label htmlFor="phone">Phone:</label>
-
+            <label htmlFor="phone">Phone:</label>
             <input
               type="text"
               id="phone"
@@ -756,10 +1040,9 @@ const TrainerSignup = () => {
             {errors.phone && touched.phone && (
               <p className="error">{errors.phone}</p>
             )}
-                        <label htmlFor="years_of_experience">Experience:</label>
-
+            <label htmlFor="years_of_experience">Experience:</label>
             <input
-              type="number" 
+              type="number"
               id="years_of_experience"
               placeholder="Years of Experience"
               value={values.years_of_experience}
@@ -775,10 +1058,9 @@ const TrainerSignup = () => {
                 className="password-container"
                 style={{ position: "relative" }}
               >
-                            <label htmlFor="passwd">Password:</label>
-
+                <label htmlFor="passwd">Password:</label>
                 <input
-                  type={showPassword ? "text" : "password"} 
+                  type={showPassword ? "text" : "password"}
                   id="passwd"
                   placeholder="Password"
                   value={values.passwd}
@@ -791,18 +1073,18 @@ const TrainerSignup = () => {
                 <button
                   type="button"
                   onClick={() => setShowPassword(!showPassword)}
-                  className="btn btn-link toggle-password" 
+                  className="btn btn-link toggle-password"
                   style={{
                     position: "absolute",
                     right: "85px",
                     top: "72%",
                     transform: "translateY(-50%)",
-                  }} 
+                  }}
                 >
                   {showPassword ? (
-                    <i className="fa-solid fa-eye-slash"></i> 
+                    <i className="fa-solid fa-eye-slash"></i>
                   ) : (
-                    <i className="fa-solid fa-eye"></i> 
+                    <i className="fa-solid fa-eye"></i>
                   )}
                 </button>
               </div>
@@ -815,10 +1097,9 @@ const TrainerSignup = () => {
                 className="password-container"
                 style={{ position: "relative" }}
               >
-                                            <label htmlFor="confirmpasswd">Confirm Password:</label>
-
+                <label htmlFor="confirmpasswd">Confirm Password:</label>
                 <input
-                  type={showConfirmPassword ? "text" : "password"} 
+                  type={showConfirmPassword ? "text" : "password"}
                   id="confirmpasswd"
                   placeholder="Confirm Password"
                   value={values.confirmpasswd}
@@ -833,18 +1114,18 @@ const TrainerSignup = () => {
                 <button
                   type="button"
                   onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-                  className="btn btn-link toggle-password" 
+                  className="btn btn-link toggle-password"
                   style={{
                     position: "absolute",
                     right: "85px",
                     top: "72%",
                     transform: "translateY(-50%)",
-                  }} 
+                  }}
                 >
                   {showConfirmPassword ? (
-                    <i className="fa-solid fa-eye-slash"></i> 
+                    <i className="fa-solid fa-eye-slash"></i>
                   ) : (
-                    <i className="fa-solid fa-eye"></i> 
+                    <i className="fa-solid fa-eye"></i>
                   )}
                 </button>
               </div>
@@ -852,16 +1133,14 @@ const TrainerSignup = () => {
             {errors.confirmpasswd && touched.confirmpasswd && (
               <p className="error">{errors.confirmpasswd}</p>
             )}
-                        
             <label className="custom-file-upload">
               <input type="file" id="photo" onChange={handleFileChange} />
               Your Image
             </label>
             <label className="custom-file-upload">
-              <input type="file" id="photo" onChange={handleFileChange} />
+              <input type="file" id="certificate" onChange={handleFileChange} />
               Your Certificate
             </label>
-
             <input
               type="submit"
               disabled={isSubmitting}
@@ -883,6 +1162,7 @@ const TrainerSignup = () => {
           </form>
         </div>
       </div>
+      <ToastContainer /> {/* Add the ToastContainer here */}
     </>
   );
 };
